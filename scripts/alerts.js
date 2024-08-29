28af4f51e1fbf62ebe09756226d6e43243749311
Kevin <kevinyahirt@gmail.com>
2024-08-28 18:10:02 -0600

    My last commit :(

--- scripts/alerts.js
+++ scripts/alerts.js
@@ -1,3 +1,30 @@
 $("#btn-1").click(function(){
-    alert("Hola mundo");
+    Swal.fire("Hola mundo");
+});
+
+$("#btn-2").click(function(){
+    Swal.fire({
+        icon:'error',
+        title:"Error",
+        text:"Texto de prueba"
+    });
+});
+
+$("#btn-3").click(function(){
+    Swal.fire({
+        imageUrl:'images/nyan-cat.gif',
+        imageHeight:300
+    });
+});
+
+$("#btn-4").click(function(){
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timerProgressBar:true,
+        toast:true,
+        timer: 1500
+      });
 });
